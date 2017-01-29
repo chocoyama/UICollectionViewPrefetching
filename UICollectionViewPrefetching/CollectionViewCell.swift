1867160f8086aa0811161ee47c838b97839eb037
Takuya Yokoyama <chocoyama@TakuyanoMacBook-Pro.local>
2017-01-29 16:39:19 +0900

    first commit

---
+++ UICollectionViewPrefetching/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  UICollectionViewPrefetching
+//
+//  Created by Takuya Yokoyama on 2017/01/29.
+//  Copyright © 2017年 Takuya Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- UICollectionViewPrefetching/ViewController.swift
+++ UICollectionViewPrefetching/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if #available(iOS 10.0, *) {
+            collectionView.prefetchDataSource = self
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        print("[DEBUG]: cellForItemAt: \(indexPath.item)\n")
+        cell.label.text = "\(indexPath.item)"
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+}
+
+extension ViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        let indexString = indexPaths.map{ "\($0.item)" }.joined(separator: " ")
+        print("[DEBUG]: prefetchItemsAt: \(indexString)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
